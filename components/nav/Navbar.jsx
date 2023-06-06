367a24570e762d772976eb72a08de6dd8c06f231
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-06-07 01:39:42 +0600

    removed flowbite dependency

--- components/nav/Navbar.jsx
+++ components/nav/Navbar.jsx
@@ -1,14 +1,49 @@
+'use client'
+
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Logo from './Logo'
 import NavbarItem from './NavbarItem'
+import { RiMenu5Line } from 'react-icons/ri'
 
-const Navbar = () => {
+const Navbar = ({ onMenuToggle }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false)
+
+  const handleBarToggle = () => {
+    const updatedMenuState = !isSideBarOpen
+    setIsSideBarOpen(updatedMenuState)
+    onMenuToggle(updatedMenuState)
+  }
+
+  useEffect(() => {
+    onMenuToggle(isSideBarOpen)
+  }, [isSideBarOpen, onMenuToggle])
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
       <div className="p-3 lg:px-5 lg:pl-3">
         <div className="flex items-center justify-between">
-          {/* Logo */}
-          <Logo />
+          <div className="flex items-center justify-start">
+            <button
+              data-drawer-target="logo-sidebar"
+              data-drawer-toggle="logo-sidebar"
+              aria-controls="logo-sidebar"
+              type="button"
+              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              onClick={handleBarToggle}
+            >
+              <span className="sr-only">Open sidebar</span>
+              <RiMenu5Line className="w-6 h-6" />
+            </button>
+            {/* Logo */}
+            <Logo />
+          </div>
           <div className="flex items-center">
             <div className="flex items-center ml-3">
               <div>
@@ -17,6 +52,7 @@ const Navbar = () => {
                   className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                   aria-expanded="false"
                   data-dropdown-toggle="dropdown-user"
+                  onClick={toggleMenu}
                 >
                   <span className="sr-only">Open user menu</span>
                   {/* User Avatar */}
@@ -30,7 +66,9 @@ const Navbar = () => {
                 </button>
               </div>
               <div
-                className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
+                className={`z-50 fixed right-2 top-9 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600 ${
+                  isMenuOpen ? '' : 'hidden'
+                }`}
                 id="dropdown-user"
               >
                 <div className="px-4 py-3" role="none">
--- components/nav/SideBar.jsx
+++ components/nav/SideBar.jsx
@@ -1,23 +1,26 @@
 'use client'
 
-import { useEffect } from 'react'
-import { initAccordions } from 'flowbite'
+import { useState } from 'react'
 import { RiApps2Line, RiInboxArchiveFill, RiUser3Fill } from 'react-icons/ri'
 import { FaHome } from 'react-icons/fa'
 import SideBarItem from './SideBarItem'
 import Navbar from './Navbar'
 
 export const SideBar = () => {
-  useEffect(() => {
-    initAccordions()
-  }, [])
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleSidebar = (isOpen) => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <>
-      <Navbar />
+      <Navbar onMenuToggle={toggleSidebar} />
       <aside
         id="logo-sidebar"
-        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+        className={`${
+          isOpen ? '-translate-x-full' : ''
+        } fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700`}
         aria-label="Sidebar"
       >
         <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
