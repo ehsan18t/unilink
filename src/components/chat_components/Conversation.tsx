31ec7b4bad7b15a5c7d2fbe26373b6241b46c787
Md Ashik <mdashik5360@gmail.com>
2023-08-15 03:06:52 +0600

    input message and submit

--- src/components/chat_components/Conversation.tsx
+++ src/components/chat_components/Conversation.tsx
@@ -30,7 +30,33 @@ const Conversation = () => {
         </div>
       </li>
     </ul>
-    
+    <footer>
+      
+        <textarea name="message" placeholder="Type your message"></textarea>
+        <span className="flex">
+          {/* <label htmlFor="file-upload" className="rounded-full p-3 bg-gray-100 hover:bg-gray-200 cursor-pointer h-5 w-5" > <Image alt="" src={AttachmentIcon} width={30} height={30}/>  </label> */}
+            <label htmlFor="fileInput" className="mr-4 cursor-pointer">
+                <svg
+                className="w-6 h-6 text-gray-500 hover:text-gray-700 transition duration-300"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                ></path>
+                </svg>
+            </label>
+          <input id="fileInput" className="invisible"type="file" name="attachment" />
+          <span  className=" mr-16 rounded-full p-3 bg-gray-100 hover:bg-gray-200" id="uploadFile">asasdfasdfasdfd</span>
+          <input className="rounded-md pr-3 bg-gray-100 hover:bg-gray-200 cursor-pointer" type="submit" name="click" value="Send" />
+          
+        </span>
+    </footer>
     
   </main>
 </div>
