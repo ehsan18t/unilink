babe5f2813286f2e9f201c7b529588df87cba134
Fuad Hasan Labib <labib.knc@gmail.com>
2023-08-15 23:59:45 +0600

    Show inside The forum post

---
+++ src/app/StudentView/ForumInside/page.tsx
@@ -0,0 +1,15 @@
+import ForumInside from "@/components/ForumInside/ForumInside";
+import ForumPostCard from "@/components/ForumPostCard/ForumPostCard";
+
+export default function Home() {
+    return (
+        <div>
+            <ForumInside/>
+
+            <ForumPostCard/>
+            <ForumPostCard/>
+            <ForumPostCard/>
+            
+        </div>
+    ) 
+  }
