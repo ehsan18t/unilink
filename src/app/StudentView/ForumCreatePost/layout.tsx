3ae05ddf95d1add873b2ee84ec1876639955d501
Fuad Hasan Labib <labib.knc@gmail.com>
2023-08-16 00:00:07 +0600

    Create Forum Post

---
+++ src/app/StudentView/ForumCreatePost/layout.tsx
@@ -0,0 +1,20 @@
+
+import { SideBar } from '@/components/nav/SideBar'
+import '@/styles/globals.css'
+
+export const metadata = {
+  title: 'Create Post',
+  description: 'This is the home page',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <SideBar />
+        {/* Content */}
+        <div className="p-4 sm:ml-64 mt-14">{children}</div>
+      </body>
+    </html>
+  )
+}
