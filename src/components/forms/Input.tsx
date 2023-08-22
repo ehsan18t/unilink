54a80dc14c8ecd75511dba41da20ca1da5385682
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-22 17:55:02 +0600

    added class passing feature to Input component

--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent } from 'react'
 import Link from 'next/link'
+import cn from 'classnames'
 
 interface Props {
   labelId: string
@@ -12,6 +13,7 @@ interface Props {
     linkUrl: string
   }
   required?: boolean
+  className?: string
 }
 
 export default function Input({
@@ -22,6 +24,7 @@ export default function Input({
   children,
   link,
   required = false,
+  className,
 }: Props) {
   return (
     <div>
@@ -46,7 +49,10 @@ export default function Input({
       <div className="mt-2">
         <input
           id={labelId}
-          className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className={cn(
+            'p-2 block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+            className,
+          )}
           name={labelId}
           type={type}
           onChange={onChange}
