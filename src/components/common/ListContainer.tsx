9db1ffb2a3a9ed79b37224d20b4f893370b6ac74
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-24 12:04:53 +0600

    removed unnecessary codes

--- src/components/common/ListContainer.tsx
+++ src/components/common/ListContainer.tsx
@@ -1,34 +1,19 @@
 'use client'
-import { use, useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import List from '@mui/material/List'
 import { useSharedList } from '@/hooks'
 
 const ListContainer = ({ items, ItemComponent }: any) => {
-  const [removedItem, setRemovedItem] = useState<any>()
   const { itemList, removeItem, updateList } = useSharedList()
 
   useEffect(() => {
     updateList(items)
   }, [items])
 
-  useEffect(() => {
-    if (removedItem) {
-      removeItem(removedItem)
-    }
-  }, [removedItem])
-
-  const handleDelete = async (item: any) => {
-    if (item) setRemovedItem(item)
-  }
-
   return (
     <div className="p-5">
       {itemList?.map((item: any) => (
-        <ItemComponent
-          key={(item as any).id}
-          item={item}
-          onDelete={(deletedItem: any | null) => handleDelete(deletedItem)}
-        />
+        <ItemComponent key={(item as any).id} item={item} />
       ))}
     </div>
   )
