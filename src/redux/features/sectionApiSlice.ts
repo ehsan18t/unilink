458e35f23045d1900db66c674fe2ee2aca157e5f
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-24 18:02:41 +0600

    Update sectionApiSlice.ts

--- src/redux/features/sectionApiSlice.ts
+++ src/redux/features/sectionApiSlice.ts
@@ -42,6 +42,7 @@ export const {
 	useRetrieveSectionsQuery,
 	useRegisterSectionMutation,
 	useUpdateSectionMutation,
+	useAddFacultyToSectionMutation,
 	useDeleteSectionMutation,
 } = sectionApiSlice;
 
