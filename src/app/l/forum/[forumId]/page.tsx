d1ad0f4834656f235d81a5c55c4b0fd62767e7d6
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-25 18:07:03 +0600

    added live search forum

--- src/app/l/forum/[forumId]/page.tsx
+++ src/app/l/forum/[forumId]/page.tsx
@@ -1,19 +1,35 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import { useParams } from 'next/navigation'
 import { useRetrieveForumPostQuery } from '@/redux/features/forumApiSlice'
 import ForumList from '@/components/page-specific/forum/ForumList'
 import Post from '@/components/page-specific/forum/Post'
 import CategoryList from '@/components/page-specific/forum/CategoryList'
+import { BsSearch } from 'react-icons/bs'
+import Button from '@/components/common/Button'
+import { ForumPost } from '@/types'
 
 const SectionList = () => {
   const params = useParams()
   const forum_id = params.forumId
+  const [searchValue, setSearchValue] = useState('')
   const {
     data: forumPosts,
     isLoading,
     isError,
   } = useRetrieveForumPostQuery(Number(forum_id))
+
+  const [postList, setPostList] = useState<any>([])
+
+  useEffect(() => {
+    if (forumPosts) {
+      const filteredPosts = forumPosts.filter((post) => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase())
+      })
+      setPostList(filteredPosts)
+    }
+  }, [searchValue, forumPosts])
 
   if (isLoading) {
     return <div>Loading...</div>
@@ -30,9 +46,23 @@ const SectionList = () => {
 
       {/* <!-- Posts (Middle Pane) --> */}
       <div className="w-1/2 bg-white border-r border-gray-100 p-4">
+        {/* Search */}
+        <div className="flex items-center justify-between mb-4">
+          <div className="flex w-9/12 items-center gap-2">
+            <input
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              type="text"
+              placeholder="Search"
+              className="border border-gray-300 rounded-md p-2 w-full"
+            />
+          </div>
+          <Button>Create Post </Button>
+        </div>
+
         <div className="h-full flex flex-col gap-5 overflow-y-scroll scrollbar-hide">
           {/* <!-- Post Messages Here --> */}
-          {forumPosts?.map((post) => (
+          {postList?.map((post: ForumPost) => (
             <Post key={post.id} post={post} />
           ))}
           {/* <!-- Post Messages Here --> */}
