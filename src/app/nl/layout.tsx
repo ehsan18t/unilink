0584cec3ccc7bf56423bf7631785fdab24efd6e8
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-27 19:52:42 +0600

    fixed require no auth

--- src/app/nl/layout.tsx
+++ src/app/nl/layout.tsx
@@ -1,24 +1,25 @@
-import '@/styles/globals.css';
-import type { Metadata } from 'next';
-import { Footer, Navbar } from '@/components/common';
+import '@/styles/globals.css'
+import type { Metadata } from 'next'
+import { Footer, Navbar } from '@/components/common'
+import RequireNoAuth from '@/components/utils/RequireNoAuth'
 
 export const metadata: Metadata = {
-	title: 'UniLink',
-	description: 'UniLink application that provides jwt authentication',
-};
+  title: 'UniLink',
+  description: 'UniLink application that provides jwt authentication',
+}
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode
 }) {
-	return (
-		<>
-			<Navbar />
-				<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8'>
-					{children}
-				</div>
-			<Footer />
-		</>
-	);
+  return (
+    <RequireNoAuth>
+      <Navbar />
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8">
+        {children}
+      </div>
+      <Footer />
+    </RequireNoAuth>
+  )
 }
