b4a3301dff007c820d25b0cd7cb68abe7c534f76
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-27 20:41:04 +0600

    updated registration logo

--- src/app/nl/auth/register/page.tsx
+++ src/app/nl/auth/register/page.tsx
@@ -11,12 +11,8 @@ export default function Page() {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="UniLink"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        <img className="mx-auto h-24 w-auto" src="/logo.png" alt="UniLink" />
+        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign up for your account
         </h2>
       </div>
