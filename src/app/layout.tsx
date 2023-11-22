9854f9176a17336c922671404049706268a511d1
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-11-22 07:26:56 +0600

    added nextui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
 import Provider from '@/redux/provider'
 import { Setup } from '@/components/utils'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,8 +21,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Provider>
-          <Setup />
-          {children}
+          <Providers>
+            <Setup />
+            {children}
+          </Providers>
         </Provider>
       </body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+import { nextui } from '@nextui-org/react'
+
+const config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/styles/**/*.css",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './src/styles/**/*.css',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      animation: {
+        'pulse-fast': 'pulse 0.7s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
     },
   },
-  plugins: [],
-};
+  darkMode: 'class',
+  plugins: [
+    nextui({
+      addCommonColors: true,
+    }),
+  ],
+}
+export default config
